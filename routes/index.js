5c9904e17bb72011bebc03d01720803e2243c2fb
jeffery shivers <jefferyshivers@gmail.com>
2017-07-31 18:28:25 -0400

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FIZZLE' });
+  res.render('index', { title: 'Off the Table' });
 });
 
 module.exports = router;
